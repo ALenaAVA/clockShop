2ec71f26342d66d09a09479a69f2b2e3fcca12a9
Alena Gontarenko <gontarenko.alena@gmail.com>
2021-01-29 15:34:11 +0200

    add block1

--- clockShop/js/main.js
+++ clockShop/js/main.js
@@ -409,7 +409,7 @@ $(document).ready(function () {
 });
 
 (function ibg() {
-	$.each($('.ibg'), function (index, val) {
+	$.each($('._ibg'), function (index, val) {
 		if ($(this).find('img').length > 0) {
 			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
 		}
