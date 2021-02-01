dc3cce43108d0839b9a36d5966e2bb0d84c32b48
Alena Gontarenko <gontarenko.alena@gmail.com>
2021-02-01 12:44:11 +0200

    add slider swiper

--- clockShop/js/main.js
+++ clockShop/js/main.js
@@ -415,3 +415,65 @@ $(document).ready(function () {
 		}
 	});
 })();
+let sliders = document.querySelectorAll('._swiper');
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		if (!slider.classList.contains('swiper-bild')) {
+			let slider_items = slider.children;
+			if (slider_items) {
+				for (let index = 0; index < slider_items.length; index++) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+
+			let slider_content = slider.innerHTML;
+
+			let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');
+		}
+
+		if (slider.classList.contains('_gallery')) {
+			//slider.data('LightGallery').destroy(true);
+		}
+	}
+	sliders_bild_callback();
+}
+function sliders_bild_callback(params) { }
+
+
+
+let slider_about = new Swiper('.main-slider__body', {
+	observer: true,
+	observeParents: true,
+	slidesPerView: 1,
+	spaceBeetween: 0,
+	//autoHeight: true,
+	//speed: 800,
+	loop: true,
+	
+	navigation: {
+		nextEl: '.control-main-slider__arrow_next',
+		prevEl: '.control-main-slider__arrow_prev',
+	},
+
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBeetween: 0,
+			autoHeight:true,
+		}
+	},
+
+	// on: {
+	// 	lazyImageReady: function () {
+	// 		ibg();
+	// 	},
+	// },
+});
